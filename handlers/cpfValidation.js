218ee3baef03ed99f78198d6071894122bd87f39
noziX <94942615+noziXsHooter@users.noreply.github.com>
2023-04-15 17:52:11 -0300

    fix vars name

--- handlers/cpfValidation.js
+++ handlers/cpfValidation.js
@@ -22,36 +22,36 @@
       return false;
     }
   
-    let soma = 0;
-    let resto;
+    let sum = 0;
+    let rest;
   
     for (let i = 1; i <= 9; i++) {
-      soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+      sum += parseInt(cpf.substring(i - 1, i)) * (11 - i);
     }
   
-    resto = (soma * 10) % 11;
+    rest = (sum * 10) % 11;
   
-    if (resto === 10 || resto === 11) {
-      resto = 0;
+    if (rest === 10 || rest === 11) {
+      rest = 0;
     }
   
-    if (resto !== parseInt(cpf.substring(9, 10))) {
+    if (rest !== parseInt(cpf.substring(9, 10))) {
       return false;
     }
   
-    soma = 0;
+    sum = 0;
   
     for (let i = 1; i <= 10; i++) {
-      soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+      sum += parseInt(cpf.substring(i - 1, i)) * (12 - i);
     }
   
-    resto = (soma * 10) % 11;
+    rest = (sum * 10) % 11;
   
-    if (resto === 10 || resto === 11) {
-      resto = 0;
+    if (rest === 10 || rest === 11) {
+      rest = 0;
     }
   
-    if (resto !== parseInt(cpf.substring(10, 11))) {
+    if (rest !== parseInt(cpf.substring(10, 11))) {
       return false;
     }
   
